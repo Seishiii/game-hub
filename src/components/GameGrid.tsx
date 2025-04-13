e622a79970d2dbe9e40fcf1facfc2ba2ec39af09
Kent Ogawa <ogawakc122@gmail.com>
2025-04-14 06:17:11 +0800

    Refactoring: Fetch games with React Query
    This reverts commit 1663b6687733a76b1a3f26f701e13df56744a1d3.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  if (error) return <Text>{error.message}</Text>;
+  if (error) return <Text>{error}</Text>;
 
   return (
     <SimpleGrid
@@ -29,7 +29,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           </GameCardContainer>
         ))}
       {isLoading ||
-        data?.results.map((game) => (
+        data.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-export interface FetchResponse<T> {
-  count: number;
-  results: T[];
-}
-
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
